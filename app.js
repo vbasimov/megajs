16b7bffde2ad5e04cfc5b1d06c512b10c9caabb2
vabsoluter <vabsoluter@gmail.com>
2018-12-14 00:31:39 +0300

    added common structure, simple POST auth

--- app.js
+++ app.js
@@ -1,14 +1,99 @@
-var express = require("express");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var uuid = require('uuid/v4');
+var session = require('express-session')
+var FileStore = require('session-file-store')(session);
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const users = [ {
+    id: 'fg0vpazo',
+    email: 'test@test.com',
+    password: 'password'
+} ]
+
+// use the local strategy
+passport.use(new LocalStrategy(
+  { usernameField: 'email' },
+  (email, password, done) => {
+    console.log('Inside local strategy callback')
+    // here is where you make a call to the database
+    // to find the user based on their username or email address
+    // for now, we'll just pretend we found that it was users[0]
+    const user = users[0] 
+    if(email === user.email && password === user.password) {
+      console.log('Local strategy returned true')
+      return done(null, user)
+    }
+  }
+));
+
+// serialize the user by passport
+passport.serializeUser((user, done) => {
+  console.log('Inside serializer callback. User id is save to the session file store here')
+  done(null, user.id);
+});
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
+
 var app = express();
 
-app.get("/", function(req, res) {
-    res.send ("<div>HOLA</div>");
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+////////
+//TODO// replace session secret to ENV
+////////
+
+//session middleware
+app.use(session({
+  genid: (req) => {
+    console.log('Inside the session middleware')
+    console.log(req.sessionID)
+    return uuid()
+  },
+  store: new FileStore(),
+  secret: 'megaphonetestapp',
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/auth', authRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.get("*", function(req, res){
-    res.send ("<h1>Attention! Wrong URL</h1>")
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-app.listen(3000, function(){
-    console.log("server works fine on port 3000");
-});
+module.exports = app;
---
+++ routes/auth.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var passport = require('passport');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  console.log('Inside GET /auth callback function');
+  console.log(req.sessionID);
+  res.render('auth', { title: 'Authorization'});
+});
+
+router.post('/', function(req, res, next) {
+  console.log('Inside POST /auth callback')
+  passport.authenticate('local', (err, user, info) => {
+    console.log('Inside passport.authenticate() callback');
+    console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
+    console.log(`req.user: ${JSON.stringify(req.user)}`)
+    req.login(user, (err) => {
+      console.log('Inside req.login() callback')
+      console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
+      console.log(`req.user: ${JSON.stringify(req.user)}`)
+      return res.send('You were authenticated & logged in!\n');
+    })
+  })(req, res, next);
+});
+
+module.exports = router;
